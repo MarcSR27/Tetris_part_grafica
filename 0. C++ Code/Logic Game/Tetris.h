9853208b814916e9723c64a0c16c74f7bffec361
NIU1709497 <148829321+NIU1709497@users.noreply.github.com>
2024-05-19 22:59:02 +0200

    Create Tetris.h

---
+++ 0. C++ Code/Logic Game/Tetris.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef TETRIS_H
+#define TETRIS_H
+
+#include "Partida.h"
+
+using namespace std;
+
+class Tetris
+{
+public:
+	void juga(); //PROVISIONAL
+	void mostraPuntuacions(); //PROVISIONAL
+
+
+private:
+	Partida m_partida;
+};
+
+void menuInicial(int& mode); //PROVISIONAL
+
+#endif;
